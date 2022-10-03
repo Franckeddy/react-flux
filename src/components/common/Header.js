be44652bbc51d466f2ff95ab848a386f18653a4e
Franckeddy <franck.garcon@gmail.com>
2022-10-03 15:25:23 +0200

    React Router

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 function Header() {
+    const activeStyle = {color : "orange"};
     return (
         <nav>
             {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
-            <a href="/">Home</a> | <a href="/courses">Courses</a> |
-            <a href="/about">About</a>
+            <NavLink activeStyle={activeStyle} exact to="/">
+                Home
+            </NavLink>
+            |
+            <NavLink activeStyle={activeStyle} to="/courses">
+                Courses
+            </NavLink>
+            |
+            <NavLink activeStyle={activeStyle} to="/about">
+                About
+            </NavLink>
         </nav>
     )
 }
